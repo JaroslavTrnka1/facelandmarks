0513072ca2e5f0430097b9b5e7894ae3c47a6cfc
Jaroslav Trnka <jtrnka@multima.cz>
2024-01-21 10:34:27 +0100

    modified:   __init__.py 	deleted:    app.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,2 @@
-# from recogniser.version import __version__, __version_date__
-# import os
-# import json
-
-# with open(f"{os.path.dirname(__file__) + '/'}config_recogniser.json", "r") as json_config:
-#     config = json.load(json_config)
-
-# MODELLING_CONFIG = config["modelling"]
-# POSTPROCESSING_CONFIG = config["postprocessing"]
-# PREPROCESSING_CONFIG = config["preprocessing"]
+from face_landmarking_model import FaceLandmarking
+MODEL_PHASE = 1
