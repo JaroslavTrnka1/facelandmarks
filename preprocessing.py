0e776022ef018976768b944dc148bc630d1f78b8
Jaroslav Trnka <jtrnka@multima.cz>
2024-06-23 09:40:53 +0000

    modified:   .gitignore 	modified:   preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -22,7 +22,7 @@ def get_sample_groups(root_dirs = None):
                 sample_groups.append(root.replace("\\","/"))
     return sample_groups
 
-def prepare_training_landmarks(both_models = BOTH_MODELS, root_dirs = None):
+def prepare_training_landmarks(both_models = BOTH_MODELS, group = None):
     if both_models:
         x = np.empty((0, 546 * 2))
     else:
@@ -30,68 +30,66 @@ def prepare_training_landmarks(both_models = BOTH_MODELS, root_dirs = None):
     y_true = np.empty((0, 144))
     
     # face_detail_coordinates = np.empty((0, 4))
-    groups = get_sample_groups(root_dirs)
+
     path_list = []
     angles = np.empty(0)
     crops = np.empty((0,4))
-    
-    for group in tqdm(groups):
 
-        for file in os.listdir(group):
-            if file[-4:] in ['.TPS', '.tps']:
-                tps = readtps(group + '/' + file, group)
+    for file in os.listdir(group):
+        if file[-4:] in ['.TPS', '.tps']:
+            tps = readtps(group + '/' + file, group)
 
-                for idx in range(len(tps['im'])):
-                    true_landmarks = tps['coords'][:, :, idx]
-                                      
-                    try:
-                        
-                        img_file = tps['im'][idx]
-                        filename, extension = img_file.split('.')
-                        if extension in ['JPG', 'BMP']:
-                            img_path = f'{group}/{filename}.{extension.lower()}'
-                            file2lowercase(img_path)
-                        else:
-                            img_path = f'{group}/{filename}.{extension}'
+            for idx in range(len(tps['im'])):
+                true_landmarks = tps['coords'][:, :, idx]
+                                    
+                try:
+                    img_file = tps['im'][idx]
+                    filename, extension = img_file.split('.')
+                    if extension in ['JPG', 'BMP']:
+                        if os.path.exists(f'{group}/{img_file}'):
+                            file2lowercase(f'{group}/{img_file}')
+                        img_path = f'{group}/{filename}.{extension.lower()}'
+                    else:
+                        img_path = f'{group}/{filename}.{extension}'
 
-                        image = cv2.imread(img_path)
-                        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                        
-                        # scaling from pixel to (0,1)
-                        # true_landmarks = np.divide(true_landmarks, (image.shape[1], image.shape[0]))
+                    image = cv2.imread(img_path)
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                    
+                    # scaling from pixel to (0,1)
+                    # true_landmarks = np.divide(true_landmarks, (image.shape[1], image.shape[0]))
 
-                        # We crop only face detail for a better precision
-                        # and accomodate landmark coordinates to cropped subimage
-                        # as well as to float (0,1) scale
-                        subimage, xmin, ymin, xmax, ymax = crop_face_only(image)
-                        true_landmarks = np.subtract(true_landmarks, (xmin, image.shape[0] - ymax))
-                        true_landmarks = np.divide(true_landmarks, (subimage.shape[1], subimage.shape[0]))
-                        # As the source true landmarks have y-origin on the bottom of picture
-                        # (unlike MediaPipe model)
-                        # we have to flip their y-axis
-                        true_landmarks[:,1] = 1 - true_landmarks[:,1]
+                    # We crop only face detail for a better precision
+                    # and accomodate landmark coordinates to cropped subimage
+                    # as well as to float (0,1) scale
+                    subimage, xmin, ymin, xmax, ymax = crop_face_only(image)
+                    true_landmarks = np.subtract(true_landmarks, (xmin, image.shape[0] - ymax))
+                    true_landmarks = np.divide(true_landmarks, (subimage.shape[1], subimage.shape[0]))
+                    # As the source true landmarks have y-origin on the bottom of picture
+                    # (unlike MediaPipe model)
+                    # we have to flip their y-axis
+                    true_landmarks[:,1] = 1 - true_landmarks[:,1]
 
-                        if both_models:
-                            input_landmarks = np.concatenate((MediaPipe_model(subimage), LBF_model(subimage)), axis = 0)
-                        else:
-                            input_landmarks = MediaPipe_model(subimage)
-                        
-                        angle = get_face_angle(input_landmarks, subimage.shape)
-                        
-                        input_landmarks = input_landmarks.reshape(1,-1)
-                        x = np.concatenate((x, input_landmarks), axis = 0)
+                    if both_models:
+                        input_landmarks = np.concatenate((MediaPipe_model(subimage), LBF_model(subimage)), axis = 0)
+                    else:
+                        input_landmarks = MediaPipe_model(subimage)
+                    
+                    angle = get_face_angle(input_landmarks, subimage.shape)
+                    
+                    input_landmarks = input_landmarks.reshape(1,-1)
+                    x = np.concatenate((x, input_landmarks), axis = 0)
 
-                        true_landmarks = true_landmarks.reshape(1,-1)
-                        y_true = np.concatenate((y_true, true_landmarks), axis = 0)
-                        
-                        angles = np.concatenate((angles, np.array([angle])))
-                        crops = np.concatenate((crops, np.array([xmin, ymin, xmax, ymax]).reshape(1,-1)), axis=0)
-                        
-                        path_list.append(img_path)
-                        
-                    except Exception as e:
-                        # Wrong image wasn't accepted into preprocessed data
-                        print(f"Preprocessing went wrong in image: {img_path}, error: {e}.")
+                    true_landmarks = true_landmarks.reshape(1,-1)
+                    y_true = np.concatenate((y_true, true_landmarks), axis = 0)
+                    
+                    angles = np.concatenate((angles, np.array([angle])))
+                    crops = np.concatenate((crops, np.array([xmin, ymin, xmax, ymax]).reshape(1,-1)), axis=0)
+                    
+                    path_list.append(img_path)
+                    
+                except Exception as e:
+                    # Wrong image wasn't accepted into preprocessed data
+                    print(f"Preprocessing went wrong in image: {img_path}, error: {e}.")
 
     return x, y_true, path_list, angles, crops
 
@@ -100,28 +98,44 @@ def prepare_training_landmarks(both_models = BOTH_MODELS, root_dirs = None):
 def save_preprocessed_data(x_inp, y_inp, path_list, angles, crops, target_folder):
     if not os.path.exists(target_folder):
         os.mkdir(target_folder)
+        
+    if os.path.exists(f'{target_folder}/path_list.txt'):
+        already_preprocessed = np.load(f'{target_folder}/preprocessed_inputs.npz')
+        already_saved_angles = np.load(f'{target_folder}/angles.npz')['angles']
+        already_saved_crops = np.load(f'{target_folder}/crops.npz')['crops']
+        all_inputs = already_preprocessed['x_inp']
+        all_targets = already_preprocessed['y_inp']
+        x_inp = np.concatenate((x_inp, all_inputs), axis = 0)
+        y_inp = np.concatenate((y_inp, all_targets), axis = 0)
+        angles = np.concatenate((angles, already_saved_angles))
+        crops = np.concatenate((crops, already_saved_crops), axis = 0)
+        
     np.savez(f'{target_folder}/preprocessed_inputs', x_inp = x_inp, y_inp = y_inp)
     np.savez(f'{target_folder}/angles', angles = angles)
     np.savez(f'{target_folder}/crops', crops = crops)
     
-    with open(f'{target_folder}/path_list.txt', "w") as pl:
+    path_file_exists = os.path.isfile(f'{target_folder}/path_list.txt')
+    with open(f'{target_folder}/path_list.txt', 'a' if path_file_exists else 'w') as file:
         for path in path_list:
-            pl.write(str(path) +"\n")
+            file.write(path + '\n')
 
 
 def preprocess_data(target_folder, data_folders):
     if not os.path.isfile(f"{target_folder}/path_list.txt"):  
-        x_inp, y_inp, path_list, angles, crops = prepare_training_landmarks(root_dirs=data_folders)   
-        try:
-            print(x_inp.shape)
-            print(y_inp.shape)
-            print(len(path_list)) 
-            print(angles.shape)
-        except Exception as e:
-            print(e)
-            
-        save_preprocessed_data(x_inp, y_inp, path_list, angles, crops, target_folder)
+        groups = get_sample_groups(data_folders)
+        for group in tqdm(groups):
+            x_inp, y_inp, path_list, angles, crops = prepare_training_landmarks(group=group)   
+            # try:
+            #     print(x_inp.shape)
+            #     print(y_inp.shape)
+            #     print(crops.shape)
+            #     print(angles.shape)
+            #     print(len(path_list)) 
+            # except Exception as e:
+            #     print(e)
+                
+            save_preprocessed_data(x_inp, y_inp, path_list, angles, crops, target_folder)
     else:
         print("Preprocessed data already exist - delete them first!")
 
-preprocess_data(target_folder="preprocessed_data", data_folders=['AI_Morphometrics', 'PHABRNO_pro_ML'])
+# preprocess_data(target_folder="preprocessed_data", data_folders=['data'])
